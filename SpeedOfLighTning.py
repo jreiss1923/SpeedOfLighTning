f7a58d236e5006b27832fc1773cab2f5114c4d0e
jreiss1923 <joshua.reiss2@gmail.com>
2022-08-15 00:09:10 -0400

    frontend works now

--- SpeedOfLighTning.py
+++ SpeedOfLighTning.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from walk_time_differences import walk_time_differences
+from walk_time_differences import walk_time_differences, train_lists, irregular_stop_name_list
 import json
 
 app = Flask(__name__)
@@ -10,8 +10,29 @@ def hello_world():
 
 @app.route('/select_line')
 def select_line():
-    t_line_value = walk_time_differences[request.args.get("list_of_lines")]
-    print(type(json.dumps(t_line_value)))
-    return json.dumps(t_line_value, ensure_ascii=True)
+    t_line_value = train_lists[request.args.get("list_of_lines")]
+    train_list = t_line_value
+    return json.dumps(t_line_value[0:-1], ensure_ascii=True).replace("'", '"')
+
+@app.route('/select_stop')
+def select_stop():
+    t_stop_value = request.args.get("first_stop_list")
+    t_line_value = train_lists[request.args.get("list_of_lines")]
+    sub_train_list = t_line_value[t_line_value.index(t_stop_value)+1:]
+    return json.dumps(sub_train_list, ensure_ascii=True).replace("'", '"')
+
+@app.route('/get_difference')
+def get_difference():
+    t_line_value = request.args.get("list_of_lines")
+    t_first_stop_value = request.args.get("first_stop_list")
+    t_second_stop_value = request.args.get("second_stop_list")
+
+    t_line_value = "walk_time_difference_" + "_".join(t_line_value.split("_")[0:-1])
+    t_first_stop_value = t_first_stop_value + "_" + t_line_value[-1]
+    t_second_stop_value = t_second_stop_value + "_" + t_line_value[-1]
+
+    walk_time_list = walk_time_differences[t_line_value]
+    time_diff = walk_time_list[t_first_stop_value + " " + t_second_stop_value]
+    return str(format(abs(time_diff/60), ".0f"))
 
 app.run()
